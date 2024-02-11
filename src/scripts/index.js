28272cb807845b07fca9df206d3e54dc6a90865a
VitalikSamusenko <vitaliksamusenko1997@gmail.com>
2024-02-11 16:04:24 +0300

    feat: added a canvas with a 3d model, also added model rotation behind the cursor

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,59 @@
-import '../assets/styles/style.scss'
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import background from "../assets/images/background.png";
+import "../assets/styles/style.scss";
+
+import init from "./init";
+
+const MODEL_URL = new URL(
+  "../assets/model/t-34-85_3d-model.glb",
+  import.meta.url
+);
+
+const { sizes, camera, scene, canvas, controls, renderer } = init();
+
+let cursorX = 0,
+  cursorY = 0;
+
+scene.background = new THREE.TextureLoader().load(background);
+
+const loader = new GLTFLoader();
+loader.load(MODEL_URL.href, (glft) => {
+  scene.add(glft.scene);
+  animate();
+});
+
+const findCursorPosition = (event) => {
+  cursorX = event.clientX - window.innerWidth / 2;
+  cursorY = event.clientY - window.innerHeight / 2;
+  console.log(cursorX, cursorY);
+};
+
+renderer.domElement.addEventListener("pointermove", findCursorPosition);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  camera.position.x += (cursorX - camera.position.x) * 0.0001;
+  camera.position.y += (-cursorY - camera.position.y) * 0.0001;
+  camera.position.z = 6;
+  camera.lookAt(scene.position);
+
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+window.addEventListener("resize", () => {
+  // Обновляем размеры
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Обновляем соотношение сторон камеры
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Обновляем renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+  renderer.render(scene, camera);
+});
---
+++ src/scripts/init.js
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+const init = () => {
+	const sizes = {
+		width: window.innerWidth,
+		height: window.innerHeight,
+	};
+
+	const scene = new THREE.Scene();
+	const canvas = document.querySelector('.canvas');
+	const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+	scene.add(camera);
+
+	const controls = new OrbitControls(camera, canvas);
+	controls.enableDamping = true;
+    controls.enableZoom = false;
+	controls.enableRotate = false;
+	
+	const renderer = new THREE.WebGLRenderer({ canvas });
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.render(scene, camera);
+	
+	const hemiLight = new THREE.HemisphereLight(0xffffff, 0x333333);
+	hemiLight.position.set(0, 30, 0);
+	scene.add(hemiLight);
+	
+	const dirLight = new THREE.DirectionalLight(0xffffff, 0, 54);
+	dirLight.position.set(-4, 12, 12);
+	dirLight.castShadow = true;
+	dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+	scene.add(dirLight);
+
+	camera.position.set(0, 1, 6);
+	
+	return { sizes, scene, canvas, camera, renderer, controls };
+};
+
+export default init;
