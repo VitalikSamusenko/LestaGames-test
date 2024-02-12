c0b94434e9e011dd251c107373df896130bac47e
Vitalik Samusenko <vitaliksamusenko1997@gmail.com>
2024-02-12 09:54:23 +0300

    feat: added icons to canvas

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,11 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-import background from "../assets/images/background.png";
 import "../assets/styles/style.scss";
+
+import background from "../assets/images/background.png";
+import armor from '../assets/images/armor.png'
+import gunAndTower from '../assets/images/gun-and-tower.png'
+import tracks from '../assets/images/tracks.png'
 
 import init from "./init";
 
@@ -9,6 +13,9 @@ const MODEL_URL = new URL(
   "../assets/model/t-34-85_3d-model.glb",
   import.meta.url
 );
+
+
+
 
 const { sizes, camera, scene, canvas, controls, renderer } = init();
 
@@ -23,10 +30,46 @@ loader.load(MODEL_URL.href, (glft) => {
   animate();
 });
 
+
+
+const gunAndTowerGeometry = new THREE.BoxGeometry( 0.6, 0.6, 0);
+const gunAndTowerMaterial = new THREE.MeshBasicMaterial( 
+    {
+        map: new THREE.TextureLoader().load(gunAndTower),
+        transparent: true
+} 
+    );
+const gunAndTowerIcon = new THREE.Mesh( gunAndTowerGeometry, gunAndTowerMaterial );
+gunAndTowerIcon.position.set(0,1.4,1.5)
+scene.add( gunAndTowerIcon );
+
+
+const armorGeometry = new THREE.BoxGeometry( 0.6, 0.6, 0);
+const armorMaterial = new THREE.MeshBasicMaterial( 
+    {
+        map: new THREE.TextureLoader().load(armor),
+        transparent: true
+} 
+    );
+const armorIcon = new THREE.Mesh( armorGeometry, armorMaterial );
+armorIcon.position.set(-1.3,0.3, 2)
+scene.add( armorIcon );
+
+
+const tracksGeometry = new THREE.BoxGeometry( 0.6, 0.6, 0);
+const tracksMaterial = new THREE.MeshBasicMaterial( 
+    {
+        map: new THREE.TextureLoader().load(tracks),
+        transparent: true
+} 
+    );
+const tracksIcon = new THREE.Mesh( tracksGeometry, tracksMaterial );
+tracksIcon.position.set(1.4,0.1,2.5)
+scene.add( tracksIcon );
+
 const findCursorPosition = (event) => {
   cursorX = event.clientX - window.innerWidth / 2;
   cursorY = event.clientY - window.innerHeight / 2;
-  console.log(cursorX, cursorY);
 };
 
 renderer.domElement.addEventListener("pointermove", findCursorPosition);
